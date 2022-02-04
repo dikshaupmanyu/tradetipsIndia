6c5d3860b192e3e83441b96552ac99c48d0144ba
Diksha Upmanyu <diksha.u@vsple.com>
2022-02-04 19:16:46 +0530

    add changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var http = require('http');
 var https = require('https');
+var fs = require('fs');
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
 var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
